bb96711409cb5e8c0fa38994df68322d04a7647d
LuizEduardo-service <luizeduardo.service@outlook.com>
2025-03-20 08:04:18 -0300

    feat: create templates and _base.html

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+from fastapi.requests import Request
 
 
 app = FastAPI(redoc_url=None, docs_url=None)
@@ -8,6 +9,10 @@ app.mount('/media', StaticFiles(directory='media'), name='media')
 app.mount('/static',StaticFiles(directory='static'), name='static')
 templates = Jinja2Templates(directory='templates')
 
+
+@app.get('/')
+async def ixdex(request: Request):
+    return templates.TemplateResponse('home/index.html',context={"request": request})
 
 if __name__ == '__main__':
     import uvicorn
